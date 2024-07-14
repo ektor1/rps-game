87a33ecc21b39dd8f910392cdf39b6ed22a524b4
ektor1 <hc.nlks@gmail.com>
2024-07-14 19:36:18 +0100

    Cleaned code - improved getComputerChoice function - improved logics for playRound function

--- game.js
+++ game.js
@@ -1,54 +1,25 @@
-// allows Math.random to be scaled so getComputerChoice returns 0, 1, or 2
-let max = 3; 
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * max);
-
-    if (choice == 0) {
-        return "rock";
-    } else if (choice == 1) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+    let choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(humanChoice, computerChoice) {
-
     if (humanChoice == computerChoice) {
         roundResultMessage.textContent = "It's a tie!";
-        // result.appendChild(roundResultMessage);
         return;
-
-    } else if (humanChoice == "rock" && computerChoice == "paper") {
-        roundResultMessage.textContent = "You lose! Paper beats Rock.";
-        // result.appendChild(roundResultMessage);
-        return 0;
-
-    } else if (humanChoice == "rock" && computerChoice == "scissors") {
-        roundResultMessage.textContent = "You win! Rock beats Scissors.";
-        // result.appendChild(roundResultMessage);
-        return 1;
-
-    } else if (humanChoice == "paper" && computerChoice == "scissors") {
-        roundResultMessage.textContent = "You lose! Scissors beats Paper.";
-        // result.appendChild(roundResultMessage);
-        return 0;
+    } else if (
+        humanChoice == "rock" && computerChoice == "scissors" || 
+        humanChoice == "paper" && computerChoice == "rock" ||
+        humanChoice == "scissors" && computerChoice == "paper") {
         
-    } else if (humanChoice == "paper" && computerChoice == "rock") {
-        roundResultMessage.textContent = "You win! Paper beats Rock.";
-        // result.appendChild(roundResultMessage);
-        return 1;
-        
-    } else if (humanChoice == "scissors" && computerChoice == "rock") {
-        roundResultMessage.textContent = "You lose! Rock beats Scissors.";
-        // result.appendChild(roundResultMessage);
-        return 0;
-
-    } else if (humanChoice == "scissors" && computerChoice == "paper") {
-        roundResultMessage.textContent = "You win! Scissors beats Paper.";
-        // result.appendChild(roundResultMessage);
-        return 1;
+        roundResultMessage.textContent = "You win! " + humanChoice + " beats " + computerChoice;
+        humanScore += 1;
+    } else {
+        roundResultMessage.textContent = "You lost! " + computerChoice + " beats " + humanChoice;
+        computerScore += 1;
     }
 }
 
@@ -59,36 +30,26 @@ let humanScoreMessage = document.querySelector("#humanScoreMessage");
 let computerScoreMessage = document.querySelector("#computerScoreMessage");
 let resultMessage = document.querySelector("#resultMessage");
 
-let humanScore = 0;
-let computerScore = 0;
-let roundsNum = 0;
-
 choices.forEach((button) => {
     button.addEventListener("click", () => {
 
-        resultMessage.textContent = "";
+        resultMessage.textContent = ""; // clean result from previous game
         const humanChoice = button.id;
         const computerChoice = getComputerChoice();
-        let roundResult = playRound(humanChoice, computerChoice);
-
-        if (roundResult == 1) {
-            humanScore += 1;
-        } else if (roundResult == 0) {
-            computerScore += 1;
-        }
+        playRound(humanChoice, computerChoice);
 
         humanScoreMessage.textContent = "Your score: " + humanScore;
         computerScoreMessage.textContent = "Computer score: " + computerScore;
 
         if (humanScore == 5 || computerScore == 5) {
             if (humanScore == computerScore) {
-                resultMessage.textContent = "This game is a tie!";
+                resultMessage.textContent += "This game is a tie!";
         
             } else if (humanScore > computerScore) {
-                resultMessage.textContent =  "Congrats, you won this game! :D";
+                resultMessage.textContent += "Congrats, you won this game! :D";
                 
             } else {
-                resultMessage.textContent =  "Computer won this game! :(";
+                resultMessage.textContent += "Computer won this game! :(";
             }
             humanScore = 0;
             computerScore = 0;
